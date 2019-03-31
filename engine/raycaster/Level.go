2b5e8554a88f5ce6413a024a6bf4cc64a7d697c3
Eric Harbeston <harbdog@users.noreply.github.com>
2019-03-31 13:53:10 -0400

    Level moved to raycaster package to avoid circular imports

---
+++ engine/raycaster/Level.go
@@ -0,0 +1,16 @@
+package raycaster
+
+import (
+	"image"
+	"image/color"
+)
+
+// Level --struct to represent rects and tints of a level--//
+type Level struct {
+	Sv  []*image.Rectangle
+	Cts []*image.Rectangle
+
+	//--current slice tint (for lighting)--//
+	St         []*color.Color
+	CurrTexNum []int
+}
