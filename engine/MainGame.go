46d689bb61fae1085be596725d28494bbbaee9a6
Eric Harbeston <harbdog@users.noreply.github.com>
2019-03-31 20:28:40 -0400

    Starting to load and draw textures (badly)

--- engine/MainGame.go
+++ engine/MainGame.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	"image/color"
 	"log"
+	"path/filepath"
 	"raycaster-go/engine/raycaster"
 	"runtime"
 
@@ -37,8 +38,7 @@ type Game struct {
 	camera *raycaster.Camera
 
 	//--graphics manager and sprite batch--//
-	//private GraphicsDeviceManager graphics;
-	//private SpriteBatch spriteBatch;
+	spriteBatch *SpriteBatch
 
 	textures [5]*ebiten.Image
 
@@ -54,6 +54,12 @@ type Game struct {
 
 	//--array of levels, levels reffer to "floors" of the world--//
 	levels []*raycaster.Level
+}
+
+// SpriteBatch - converted C# method Graphics.SpriteBatch
+// Enables a group of sprites to be drawn using the same settings.
+type SpriteBatch struct {
+	g *Game
 }
 
 // NewGame - Allows the game to perform any initialization it needs to before starting to run.
@@ -81,8 +87,37 @@ func NewGame() *Game {
 	return g
 }
 
+// loadContent will be called once per game and is the place to load
+// all of your content.
+func (g *Game) loadContent() {
+	// Create a new SpriteBatch, which can be used to draw textures.
+	g.spriteBatch = &SpriteBatch{g: g}
+
+	// TODO: use this.Content to load your game content here
+	g.textures[0], _, _ = getTextureFromFile("stone.png")
+	g.textures[1], _, _ = getTextureFromFile("left_bot_house.png")
+	g.textures[2], _, _ = getTextureFromFile("right_bot_house.png")
+	g.textures[3], _, _ = getTextureFromFile("left_top_house.png")
+	g.textures[4], _, _ = getTextureFromFile("right_top_house.png")
+
+	g.floor, _, _ = getTextureFromFile("floor.png")
+	g.sky, _, _ = getTextureFromFile("sky.png")
+}
+
+func getTextureFromFile(texFile string) (*ebiten.Image, image.Image, error) {
+	resourcePath := filepath.Join("engine", "content", "textures")
+	eImg, iImg, err := ebitenutil.NewImageFromFile(filepath.Join(resourcePath, texFile), ebiten.FilterNearest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return eImg, iImg, err
+}
+
 // Run is the Ebiten Run loop caller
 func (g *Game) Run() {
+	// load content once when first run
+	g.loadContent()
+
 	// On browsers, let's use fullscreen so that this is playable on any browsers.
 	// It is planned to ignore the given 'scale' apply fullscreen automatically on browsers (#571).
 	if runtime.GOARCH == "js" || runtime.GOOS == "js" {
@@ -94,6 +129,8 @@ func (g *Game) Run() {
 	}
 }
 
+// Update - Allows the game to run logic such as updating the world,
+// checking for collisions, gathering input, and playing audio.
 func (g *Game) Update(screen *ebiten.Image) error {
 	g.view = screen
 
@@ -109,6 +146,9 @@ func (g *Game) Update(screen *ebiten.Image) error {
 		fmt.Printf("mouse right clicked: (%v, %v)\n", mx, my)
 	}
 
+	// TODO: Add your update logic here
+	g.camera.Update()
+
 	if ebiten.IsDrawingSkipped() {
 		// When the game is running slowly, the rendering result
 		// will not be adopted.
@@ -116,11 +156,52 @@ func (g *Game) Update(screen *ebiten.Image) error {
 	}
 
 	// Render game to screen
+	g.draw()
+
 	// TPS counter
 	fps := fmt.Sprintf("TPS: %f/%v", ebiten.CurrentTPS(), ebiten.MaxTPS())
 	ebitenutil.DebugPrint(g.view, fps)
 
 	return nil
+}
+
+func (g *Game) draw() {
+	g.view.Clear()
+
+	//--draw sky and floor--//
+	texRect := image.Rect(0, 0, texSize, texSize)
+	whiteRGBA := &color.RGBA{0, 0, 0, 255}
+
+	// spriteBatch.Begin();
+
+	// spriteBatch.Draw(floor,
+	//    new Rectangle(0, (int)(height * 0.5f), width, (int)(height * 0.5f)),
+	//    new Rectangle(0, 0, texSize, texSize),
+	//    Color.White);
+	floorRect := image.Rect(0, int(float64(g.height)*0.5), g.width, int(float64(g.height)*0.5))
+	g.spriteBatch.draw(g.floor, &floorRect, &texRect, whiteRGBA)
+
+	// spriteBatch.Draw(sky,
+	//    new Rectangle(0, 0, width, (int)(height * 0.5f)),
+	//    new Rectangle(0, 0, texSize, texSize),
+	//    Color.White);
+	skyRect := image.Rect(0, 0, g.width, int(float64(g.height)*0.5))
+	g.spriteBatch.draw(g.sky, &skyRect, &texRect, whiteRGBA)
+
+	// spriteBatch.End();
+
+	// //--draw walls--//
+	// spriteBatch.Begin();
+
+	// for (int x = 0; x < width; x++)
+	// {
+	// 	for (int i = levels.Length - 1; i >= 0; i--)
+	// 	{
+	// 		spriteBatch.Draw(textures[levels[i].currTexNum[x]], levels[i].sv[x], levels[i].cts[x], levels[i].st[x]);
+	// 	}
+	// }
+
+	// spriteBatch.End();
 }
 
 //returns an initialised Level struct
@@ -154,4 +235,17 @@ func (g *Game) sliceView() []*image.Rectangle {
 	}
 
 	return arr
+}
+
+func (s *SpriteBatch) draw(texture *ebiten.Image, destinationRectangle *image.Rectangle, sourceRectangle *image.Rectangle, color *color.RGBA) {
+	op := &ebiten.DrawImageOptions{}
+	op.Filter = ebiten.FilterLinear
+
+	// TODO: if destinationRectangle is not the same size as sourceRectangle, scale to fit
+	op.GeoM.Translate(float64(destinationRectangle.Min.X), float64(destinationRectangle.Min.Y))
+
+	// TODO: color channel modulation/tinting
+
+	view := s.g.view
+	view.DrawImage(texture, op)
 }
