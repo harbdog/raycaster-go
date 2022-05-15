34e54c823b8e5ebe1dcb6f61f0232976e3e37d2b
Eric H <harbdog@users.noreply.github.com>
2022-05-14 21:13:10 -0400

    Refactored map to model and sprite initialization to engine

--- engine/model/map.go
+++ engine/model/map.go
@@ -1,23 +1,17 @@
-package raycaster
+package model
 
 import (
 	"raycaster-go/engine/geom"
-	"raycaster-go/engine/model"
 )
 
 type Map struct {
 	worldMap [][]int
 	midMap   [][]int
 	upMap    [][]int
-
-	sprite []*model.Sprite
-
-	tex *TextureHandler
 }
 
-func NewMap(tex *TextureHandler) *Map {
+func NewMap() *Map {
 	m := &Map{}
-	m.tex = tex
 
 	m.worldMap = [][]int{
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -101,88 +95,6 @@ func NewMap(tex *TextureHandler) *Map {
 	}
 
 	return m
-}
-
-func (m *Map) LoadSprites() {
-	m.sprite = []*model.Sprite{
-		// // sorcerer
-		model.NewAnimatedSprite(20, 11.5, 1.4, 5, m.tex.Textures[15], 10, 1, 256, 32.0/256.0), // FIXME: 256 should come from g.texSize
-
-		// // line of trees for testing in front of initial view
-		// Setting CollisionRadius=0 to disable collision checks against small trees
-		//model.NewSprite(19.5, 11.5, m.tex.Textures[10], 256, 0), // FIXME: commented out because trapping moving sprite inside its collision radius
-		model.NewSprite(17.5, 11.5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(15.5, 11.5, m.tex.Textures[9], 256, 0),
-		// // render a forest!
-		model.NewSprite(11.5, 1.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(12.5, 1.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(132.5, 1.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(11.5, 2, m.tex.Textures[9], 256, 0),
-		model.NewSprite(12.5, 2, m.tex.Textures[9], 256, 0),
-		model.NewSprite(13.5, 2, m.tex.Textures[9], 256, 0),
-		model.NewSprite(11.5, 2.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(12.25, 2.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(13.5, 2.25, m.tex.Textures[9], 256, 0),
-		model.NewSprite(11.5, 3, m.tex.Textures[9], 256, 0),
-		model.NewSprite(12.5, 3, m.tex.Textures[9], 256, 0),
-		model.NewSprite(13.25, 3, m.tex.Textures[9], 256, 0),
-		model.NewSprite(10.5, 3.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(11.5, 3.25, m.tex.Textures[9], 256, 0),
-		model.NewSprite(12.5, 3.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(13.25, 3.5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(10.5, 4, m.tex.Textures[9], 256, 0),
-		model.NewSprite(11.5, 4, m.tex.Textures[9], 256, 0),
-		model.NewSprite(12.5, 4, m.tex.Textures[9], 256, 0),
-		model.NewSprite(13.5, 4, m.tex.Textures[14], 256, 0),
-		model.NewSprite(10.5, 4.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(11.25, 4.5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(12.5, 4.5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(13.5, 4.5, m.tex.Textures[10], 256, 0),
-		model.NewSprite(14.5, 4.25, m.tex.Textures[14], 256, 0),
-		model.NewSprite(10.5, 5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(11.5, 5, m.tex.Textures[9], 256, 0),
-		model.NewSprite(12.5, 5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(13.25, 5, m.tex.Textures[10], 256, 0),
-		model.NewSprite(14.5, 5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(11.5, 5.5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(12.5, 5.25, m.tex.Textures[10], 256, 0),
-		model.NewSprite(13.5, 5.25, m.tex.Textures[10], 256, 0),
-		model.NewSprite(14.5, 5.5, m.tex.Textures[10], 256, 0),
-		model.NewSprite(15.5, 5.5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(11.5, 6, m.tex.Textures[14], 256, 0),
-		model.NewSprite(12.5, 6, m.tex.Textures[10], 256, 0),
-		model.NewSprite(13.25, 6, m.tex.Textures[10], 256, 0),
-		model.NewSprite(14.25, 6, m.tex.Textures[10], 256, 0),
-		model.NewSprite(15.5, 6, m.tex.Textures[14], 256, 0),
-		model.NewSprite(12.5, 6.5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(13.5, 6.25, m.tex.Textures[10], 256, 0),
-		model.NewSprite(14.5, 6.5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(12.5, 7, m.tex.Textures[14], 256, 0),
-		model.NewSprite(13.5, 7, m.tex.Textures[10], 256, 0),
-		model.NewSprite(14.5, 7, m.tex.Textures[14], 256, 0),
-		model.NewSprite(13.5, 7.5, m.tex.Textures[14], 256, 0),
-		model.NewSprite(13.5, 8, m.tex.Textures[14], 256, 0),
-	}
-}
-
-func (m *Map) AppendSprite(sprite *model.Sprite) {
-	m.sprite = append(m.sprite, sprite)
-}
-
-func (m *Map) GetAt(x, y int) int {
-	return m.worldMap[x][y]
-}
-
-func (m *Map) GetSprites() []*model.Sprite {
-	return m.sprite
-}
-
-func (m *Map) GetSprite(index int) *model.Sprite {
-	return m.sprite[index]
-}
-
-func (m *Map) GetNumSprites() int {
-	return len(m.sprite)
 }
 
 func (m *Map) GetGrid() [][]int {
